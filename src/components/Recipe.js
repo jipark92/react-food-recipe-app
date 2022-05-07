735ac9764670bd94ebe476de9bc9f4b4490e9036
Ji Park <jiyongprk@gmail.com>
2022-05-07 14:12:57 -0400

    inpurt/search button and api display function working

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,47 +1,56 @@
 import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useEffect, useState} from 'react'
+import RecipeCard from './RecipeCard';
+import axios from 'axios'
 
 export default function Recipe() {
 
     const [searchValue, setSearchValue] = useState();
-    const [showResult, setShowResult] = useState();
+    const [loading, setLoading] = useState(false)
+    const [showResult, setShowResult] = useState("");
     const [datas, setData] = useState([])
 
-    const getRecipeApi = (food) => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
-        .then(response=>{
-            return response.json()
-        })
-        .then(response=>{
-            showInfo(response)
-        })
-        .catch(()=>{
-            alert('no food found')
-        })
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
+
+    const fetchRecipe = async () => {
+        setLoading(true)
+        try {
+            const res = await fetch(url)
+            const recipes = await res.json()
+            setData(recipes.meals)
+            console.log(recipes.meals)
+        } catch (error) {
+            alert("no result")
+        }
     }
 
-    const showInfo =(res)=>{
 
-    }
+    // useEffect(()=>{
+    //     fetchRecipe()
+    // },[searchValue])
 
-    const searchBtn = () => {
-        getRecipeApi(searchValue)
-    }
+
 
     const handleSearchValue = (e) =>{ 
         setSearchValue(prevSearchValue => prevSearchValue = e.target.value)
+    }
+
+    const searchBtn = () =>{
+        fetchRecipe()
+        console.log(datas)
+
     }
 
     return (
         <div className="recipe-container">
             <div className='description-container'>
                 <h1>Find Your Meal Ingredients</h1>
-                <p>WE got ingredients for everything</p>
+                <p>We got ingredients for everything</p>
             </div>
 
             <div className="search-container">
-                <Form.Control type="text" placeholder="Normal text" onChange={handleSearchValue} />
+                <Form.Control className='search-box' type="text" placeholder="Normal text"  onChange={handleSearchValue} />
                 <Button variant="primary" size="md" onClick={searchBtn}>Search</Button>
             </div>
 
@@ -50,9 +59,38 @@ export default function Recipe() {
                     <h2>Your Search Results:</h2>
                 </div>
                 <div className='result-info-container'>
-                    <p>{showResult}</p>
+                    {datas.map(data=>{
+                        return (
+                            <div className='recipe-card' key={data.idMeal}>
+                                <img src={data.strMealThumb} alt="food-pic"/>
+                                <p>{data.strMeal}</p>
+                                
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
     )
 }
+
+
+    // const getRecipeApi = (food) => {
+    //     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
+    //     .then(response=>{
+    //         return response.json()
+    //     })
+    //     .then(response=>{
+    //     })
+    //     .catch(()=>{
+    //         alert('no food found')
+    //     })
+    // }
+
+    // if(loading){
+    //     return(
+    //         <div>
+    //             loading
+    //         </div>
+    //     )
+    // }
---
+++ src/components/RecipeCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function RecipeCard(props) {
+
+    // console.log(props.RecipeDatas.meals)
+    return (
+        <div>
+            {/* Recipe Carddd */}
+            {/* {props.RecipeDatas.meals.map(item=>{
+                return (
+                    <div>{item.meals}</div>
+                )
+            })} */}
+            
+        </div>
+    )
+}
